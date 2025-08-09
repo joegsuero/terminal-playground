46fd8a40f81e600aad0189ea5599602cb4904027
joegsuero <garciasueroj@gmail.com>
2025-08-09 12:51:52 -0400

    Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/terminal-playground/" : "/",
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
   },
 }));
